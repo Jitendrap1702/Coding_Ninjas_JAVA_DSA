778af67c90fe4117e72b50fb6dfb152de5011790
jitendrap1702 <jitendrap1702@gmail.com>
2022-02-13 17:34:03 +0530

    DP2 : min num of chocolates

---
+++ src/com/company/DSA/DynamicProgramming2/Assignment/MinimumNumberOfChocolates.java
@@ -0,0 +1,12 @@
+package com.company.DSA.DynamicProgramming2.Assignment;
+
+public class MinimumNumberOfChocolates {
+
+//    public static int getMin(int arr[], int N){
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
