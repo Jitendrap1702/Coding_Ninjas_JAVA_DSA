8b3b2b035973bdd24d28616200965b9c0977238a
jitendrap1702 <jitendrap1702@gmail.com>
2022-02-14 12:43:08 +0530

    codechef : feb long challenge 2

--- src/com/company/Codechef/FebruaryLongChallenge2/XorPalindrome.java
+++ src/com/company/Codechef/FebruaryLongChallenge2/XorPalindrome.java
@@ -6,27 +6,34 @@ public class XorPalindrome {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++){
+        try {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            for (int i = 0; i < t; i++) {
 
-            int n = sc.nextInt();
-            String s = sc.next();
-            int ones = 0, zeros = 0;
-            for (int j = 0; j < n; j++){
-                if (s.charAt(j) == '0'){
-                    zeros++;
+                int n = sc.nextInt();
+                String s = sc.next();
+                int ones = 0, zeros = 0;
+                for (int j = 0; j < n; j++) {
+                    if (s.charAt(j) == '0') {
+                        zeros++;
+                    } else {
+                        ones++;
+                    }
+                }
+
+                if (n%2 == 0){
+                    if (zeros == ones){
+                        System.out.println("YES");
+                    }else if (zeros%2==0 && ones%2==0){
+                        System.out.println("YES");
+                    }else {
+                        System.out.println("NO");
+                    }
                 }else {
-                    ones++;
+                    System.out.println("YES");
                 }
             }
-
-            if ((zeros%2==0 && ones%2!=0) || (zeros%2!=0 && ones%2==0)){
-                System.out.println("YES");
-            }
-            else if (zeros%2 != 0 || ones%2 != 0){
-                System.out.println("NO");
-            }
-        }
+        }catch (Exception e){}
     }
 }
