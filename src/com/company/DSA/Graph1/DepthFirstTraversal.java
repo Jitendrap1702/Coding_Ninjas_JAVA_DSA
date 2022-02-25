8b32ea4734597ef7f360ca4c47a123d2db567c45
jitendrap1702 <jitendrap1702@gmail.com>
2022-02-25 17:31:51 +0530

    Graph : Depth first traversal

---
+++ src/com/company/DSA/Graph1/DepthFirstTraversal.java
@@ -0,0 +1,64 @@
+package com.company.DSA.Graph1;
+
+import java.util.Scanner;
+
+public class DepthFirstTraversal {
+
+    public static void dFTraversal(int[][] adjMatrix, int currentVertex){
+
+        System.out.print(currentVertex + " ");
+        for (int i = currentVertex; i < adjMatrix.length; i++){ // run loop for non traversed vertex only
+            // call for all adjacent vertex of current vertex
+            if (adjMatrix[currentVertex][i] == 1){
+                dFTraversal(adjMatrix, i);
+            }
+        }
+    }
+
+    // By taking visited array
+    public static void dFTraversal2(int[][] adjMatrix, int currentVertex, boolean[] visited){
+
+        visited[currentVertex] = true;
+        System.out.print(currentVertex + " ");
+        for (int i = 0; i < adjMatrix.length; i++){ // run loop for non traversed vertex only
+            // call for all adjacent vertex of current vertex
+            if (adjMatrix[currentVertex][i] == 1 && visited[i] == false){
+                dFTraversal2(adjMatrix, i, visited);
+            }
+        }
+    }
+
+    public static void dFTraversal(int[][] adjMatrix){
+
+        boolean[] visited = new boolean[adjMatrix.length];
+        // take 0 as current vertex
+        dFTraversal2(adjMatrix, 0, visited);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt(); // no. of vertices
+        int e = sc.nextInt(); // no. of edges
+        // assumption : if v = 4 then vertices will be : (0, 1, 2, 3)
+        int[][] adjMatrix = new int[v][v];
+        // take all edges input
+        for (int i = 0; i < e; i++){
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+            adjMatrix[v1][v2] = 1;
+            adjMatrix[v2][v1] = 1;
+        }
+        // print adjMatrix
+        for (int i = 0; i < v; i++){
+            for (int j = 0; j < v; j++){
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Depth First Traversal
+        System.out.println("Depth First Traversal : ");
+        dFTraversal(adjMatrix);
+    }
+}
